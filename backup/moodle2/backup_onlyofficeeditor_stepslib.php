0ff77069ada05ee37c67b880230918b486795b3e
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2022-02-01 00:11:38 +0300

    add backup api

---
+++ backup/moodle2/backup_onlyofficeeditor_stepslib.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     mod_onlyofficeeditoreditor
+ * @subpackage
+ * @copyright   2022 Ascensio System SIA <integration@onlyoffice.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Define all the backup steps that will be used by the backup_onlyofficeeditor_activity_task
+ */
+
+/**
+ * Define the complete choice structure for backup, with file and id annotations
+ */
+class backup_onlyofficeeditor_activity_structure_step extends backup_activity_structure_step {
+
+    protected function define_structure() {
+        // To know if we are including userinfo.
+        $userinfo = $this->get_setting_value('userinfo');
+
+        // Define each element separated.
+        $onlyofficeeditor = new backup_nested_element('onlyofficeeditor', ['id'], [
+            'name', 'intro', 'introformat', 'timecreated', 'timemodified',
+            'display', 'displayoptions', 'permissions', 'documentkey',
+        ]);
+
+        // Define sources.
+        $onlyofficeeditor->set_source_table('onlyofficeeditor', ['id' => backup::VAR_ACTIVITYID]);
+
+        // Define file annotations.
+        $onlyofficeeditor->annotate_files('mod_onlyofficeeditor', 'intro', null);
+
+        // Return the root element (instance), wrapped into standard activity structure.
+        return $this->prepare_activity_structure($onlyofficeeditor);
+    }
+}
