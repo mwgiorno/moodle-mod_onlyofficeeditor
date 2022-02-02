b40c823e8424f27b133903a06831bfa9da2ce189
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2022-02-02 20:55:14 +0300

    missed copyrights

--- classes/editor.php
+++ classes/editor.php
@@ -37,7 +37,7 @@ use Firebase\JWT\JWT;
  *
  * @package     mod_onlyofficeeditor
  * @subpackage
- * @copyright   2021 Ascensio System SIA <integration@onlyoffice.com>
+ * @copyright   2022 Ascensio System SIA <integration@onlyoffice.com>
  * @copyright   based on work by 2018 Olumuyiwa <muyi.taiwo@logicexpertise.com>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
