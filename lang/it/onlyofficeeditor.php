ebdc264e2a2744a33b0f3d417aaf6e44f5859828
Oleg Sinizin <oleg.sinizin@onlyoffice.com>
2022-04-27 18:48:23 +0300

    add files with translations

---
+++ lang/it/onlyofficeeditor.php
@@ -0,0 +1,82 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'onlyofficeeditor', language 'it'.
+ *
+ * @package     mod_onlyofficeeditor
+ * @subpackage
+ * @copyright   2022 Ascensio System SIA <integration@onlyoffice.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['docxformname'] = 'Documento';
+$string['pptxformname'] = 'Presentazione';
+$string['xlsxformname'] = 'Foglio di calcolo';
+$string['docxfformname'] = 'Modello di modulo';
+$string['uploadformname'] = '';
+$string['modulename'] = '';
+$string['modulenameplural'] = '';
+$string['modulename_help'] = '';
+$string['pluginname'] = '';
+$string['pluginadministration'] = '';
+$string['onlyofficename'] = '';
+
+$string['onlyofficeactivityicon'] = '';
+$string['onlyofficeeditor:addinstance'] = '';
+$string['onlyofficeeditor:view'] = '';
+
+$string['documentserverurl'] = '';
+$string['documentserverurl_desc'] = '';
+$string['documentserversecret'] = '';
+$string['documentserversecret_desc'] = '';
+$string['allowedformats'] = '';
+$string['allowedformats_desc'] = '';
+
+$string['selectfile'] = '';
+$string['printintro'] = '';
+$string['printintroexplain'] = '';
+$string['documentpermissions'] = '';
+$string['download'] = '';
+$string['download_help'] = '';
+$string['download_desc'] = '';
+$string['print'] = '';
+$string['print_help'] = '';
+$string['print_desc'] = '';
+
+$string['returntodocument'] = '';
+$string['docserverunreachable'] = '';
+$string['privacy:metadata'] = '';
+$string['privacy:metadata:onlyofficeeditor:userid'] = '';
+$string['privacy:metadata:onlyofficeeditor:intro'] = '';
+$string['privacy:metadata:onlyofficeeditor:introformat'] = '';
+$string['privacy:metadata:onlyofficeeditor:permissions'] = '';
+$string['privacy:metadata:onlyofficeeditor:name'] = '';
+$string['privacy:metadata:onlyofficeeditor:course'] = '';
+$string['privacy:metadata:onlyofficeeditor'] = '';
+$string['privacy:metadata:onlyofficeeditor:core_files'] = '';
+$string['forcesave'] = '';
+$string['editor_view'] = '';
+$string['editor_view_chat'] = '';
+$string['editor_view_help'] = '';
+$string['editor_view_header'] = '';
+$string['editor_view_feedback'] = '';
+$string['editor_view_toolbar'] = '';
+
+$string['oldversion'] = '';
+$string['saveaserror'] = '';
+$string['saveassuccess'] = '';
+$string['saveastitle'] = '';
+$string['saveasbutton'] = '';
