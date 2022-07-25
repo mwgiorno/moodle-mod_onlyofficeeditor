323d3677ebf0746534f879e2812426281ce9fd83
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2022-07-25 18:04:24 +0300

    plugin supports moodle2 backup

--- lib.php
+++ lib.php
@@ -45,6 +45,8 @@ function onlyofficeeditor_supports($feature) {
     switch ($feature) {
         case FEATURE_SHOW_DESCRIPTION:
             return true;
+        case FEATURE_BACKUP_MOODLE2:
+            return true;
         default:
             return null;
     }
